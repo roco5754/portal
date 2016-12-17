20c9abc25c185b7362750fa1db0a0c1c1a8754df
Devan <devan@cvg.io>
2016-12-16 18:49:40 -0700

    Support for user login with devise.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources :properties
+  root to: "properties#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
